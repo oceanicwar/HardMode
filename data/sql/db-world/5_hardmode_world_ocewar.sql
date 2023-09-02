828dd89c92141b8eb2e6f62045e9c1cdc0e276f0
jshplayer <jshplayer@outlook.com>
2023-09-02 18:45:37 +1200

    Adjusting PvP restrictions to HC

--- data/sql/db-world/5_hardmode_world_ocewar.sql
+++ data/sql/db-world/5_hardmode_world_ocewar.sql
@@ -4,19 +4,19 @@ INSERT INTO `hardmode_auras` (`mode`, `aura`, `description`) VALUES
 
 DELETE FROM `hardmode_modes`;
 INSERT INTO `hardmode_modes` (`id`, `name`, `description`, `restrictions`, `enabled`) VALUES
-	(1, '|cffff0000Hardcore|r', 'Permadeath,no RDF', 155640, 1),
-	(2, '|cff00ff00Self Crafted|r', 'Self Crafted items only', 147708, 1),
-	(3, '|cffffff00Slow XP|r', 'Retail 1x XP and group within 3 levels', 131074, 1),
-    (4, '|cffff8000Hell|r', 'Half size,speed,damage,no talents,low drop rate', 966904, 1);
+	(1, '|cffff0000Hardcore|r', 'Permadeath', 1204216, 1),
+	(2, '|cff00ff00Self Crafted|r', 'Self Crafted', 147708, 1),
+	(3, '|cffffff00Slow XP|r', 'Retail XP', 131074, 1),
+    (4, '|cffff8000Hell|r', 'No talents with poor loot and half size speed and damage', 966904, 1);
 
 DELETE FROM `hardmode_rewards`;
 INSERT INTO `hardmode_rewards` (`mode`, `reward_level`, `reward_type`, `reward_id`, `reward_count`, `comment`) VALUES
-	(1, 80, 0, 37711, 2000, 'Hardcore - Item - Oceanic War Coin'),
+	(1, 80, 0, 37711, 3000, 'Hardcore - Item - Oceanic War Coin'),
 	(1, 80, 0, 44164, 1, 'Hardcore - Item - Reins of the Black Proto-Drake'),
 	(1, 80, 1, 141, 0, 'Hardcore - Title - The Immortal'),
-	(2, 80, 0, 37711, 1000, 'SelfCraft - Item - Oceanic War Coin'),
+	(2, 80, 0, 37711, 2000, 'SelfCraft - Item - Oceanic War Coin'),
 	(2, 80, 0, 54811, 1, 'SelfCraft - Item - Celestial Steed'),
 	(2, 80, 1, 128, 0, 'SelfCraft - Title - Flawless Victor'),
 	(3, 80, 0, 13325, 1, 'SlowXP - Item - Fluorescent Green Mechanostrider'),
-	(3, 80, 0, 37711, 500, 'SlowXP - Item - Oceanic War Coin'),
+	(3, 80, 0, 37711, 1000, 'SlowXP - Item - Oceanic War Coin'),
 	(3, 80, 1, 46, 0, 'SlowXP - Title - Scarab Lord');
